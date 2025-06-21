6d48efe3a6269b532c0aa527d683cba75f8d9604
CRISTHIAN-ODL <cristhian.codl.10@gmail.com>
2025-06-21 15:40:19 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 ssh -o StrictHostKeyChecking=no azureuser@172.171.241.176 <<HTML
 
-  cd "/g/Proyectos/Aprendo/Github Actions/practicing/practicas-ga"
+  cd /home/azureuser/practicas-ga
 
   git pull --rebase origin main
 
