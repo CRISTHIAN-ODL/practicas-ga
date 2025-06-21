cf45b8ba9d36a8481a95e2c04543cbacb1149d2b
CRISTHIAN-ODL <cristhian.codl.10@gmail.com>
2025-06-21 15:21:47 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,7 @@
-# BRANCH=$1
-
-# if [ "$BRANCH" == "main" ]; then
-#   DEPLOY_PATH="/home/azureuser/practicas-ga"
-# else
-#   DEPLOY_PATH="/home/azureuser/stage"
-# fi
-
-
 ssh -o StrictHostKeyChecking=no azureuser@172.171.241.176 <<HTML
 
   cd /home/azureuser/practicas-ga
 
-  git pull --rebase origin $BRANCH
+  git pull --rebase origin main
 
 HTML
